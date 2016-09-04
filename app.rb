b2cfb4836ad63188f05191890635f1a5d682899e
Wim <wiiiim@gmail.com>
2016-09-04 17:32:03 +1200

    first commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'pdfkit'
+
+CRAGS_PRINT_URL = "http://crags.nz/print/"
+
+get '/pdf/:id' do |node_id|
+
+	PDFKit.configure do |config|
+	  config.default_options = {
+	    :margin_top => "4mm",
+	    :margin_right => "4mm",
+	    :margin_bottom => "4mm",
+	    :margin_left => "4mm"
+	   }
+  end 
+
+  kit = PDFKit.new(CRAGS_PRINT_URL + node_id)
+
+	content_type 'application/pdf'
+  kit.to_pdf
+
+end
